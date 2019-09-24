42f5cb3900baf8d1ebb8ff5944301698d4d3ac5d
Pushkar Tandon <tandonpushkar@gmail.com>
2019-09-25 02:34:46 +0530

    Adding About Component

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, { Fragment } from 'react';
 
-export default class About extends Component {
-    render() {
-        return (
-            <div>
-                About
-            </div>
-        )
-    }
-}
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About This App</h1>
+            <p>App to search Github users</p>
+            <p>Version: 1.0.0</p>
+        </Fragment>
+    );
+};
+
+export default About;
