bb1efa109320bd484767077abc7f9eb80bd25052
Pushkar Tandon <tandonpushkar@gmail.com>
2019-09-25 02:30:56 +0530

    Updates

---
+++ precache-manifest.cb8abbc42bb5ee90623088cae4f72fec.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a48a0683cb84e60c7545ec5376abeb",
+    "url": "/GithubUserFinder/index.html"
+  },
+  {
+    "revision": "53edf7d0a189d465675a",
+    "url": "/GithubUserFinder/static/js/2.922e266b.chunk.js"
+  },
+  {
+    "revision": "a8cec6b4b9459758d61f",
+    "url": "/GithubUserFinder/static/js/main.7f5d2d4c.chunk.js"
+  },
+  {
+    "revision": "cb4f902a4f722e098d5a",
+    "url": "/GithubUserFinder/static/js/runtime-main.2cf4f986.js"
+  }
+]);
